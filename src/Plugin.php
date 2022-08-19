90871ad8811e2a9e17751202ad2c0eacf710a10a
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-08-19 16:15:26 +1000

    Plugin now installable

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace spicyweb\blockbatchactions;
+
+use craft\base\Plugin as BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @package spicyweb\blockbatchactions
+ * @author Spicy Web <plugins@spicyweb.com.au>
+ * @since 1.0.0
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        parent::init();
+    }
+}
