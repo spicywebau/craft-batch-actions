3755610df28b136646137ee9635407b69fcf1040
Scandella Cédric <cedric@scandella.fr>
2023-02-20 22:45:45 +0100

    Add French tanslations

---
+++ src/translations/fr/batch-actions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Are you sure you want to delete the selected blocks?' => 'Voulez-vous vraiment supprimer les blocs sélectionnés ?',
+    'Collapse' => 'Réduire',
+    'Copy' => 'Copier',
+    'Delete' => 'Supprimer',
+    'Disable' => 'Désactiver',
+    'Enable' => 'Activer',
+    'Expand' => 'Déplier',
+    'Paste' => 'Coller',
+    'Select all' => 'Tout sélectionner',
+];
