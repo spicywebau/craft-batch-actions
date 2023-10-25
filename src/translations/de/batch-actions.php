77caf4deda237196d0642bee6fa849aa1cdb58c8
Alexander M. Korn <dev@spume.net>
2023-10-25 09:12:50 +0200

    Add German translations

---
+++ src/translations/de/batch-actions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Are you sure you want to delete the selected blocks?' => 'Sind Sie sicher, dass Sie die ausgewählten Blöcke löschen möchten?',
+    'Collapse' => 'Einklappen',
+    'Copy' => 'Kopieren',
+    'Delete' => 'Löschen',
+    'Disable' => 'Deaktivieren',
+    'Enable' => 'Aktivieren',
+    'Expand' => 'Ausklappen',
+    'Paste' => 'Einfügen',
+    'Select all' => 'Alle auswählen',
+];
